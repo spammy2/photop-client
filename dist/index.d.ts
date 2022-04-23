404d1c424f0ff585cd009f02e5e6cd4efb96ae3a
workk <pleasedontspammail@gmail.com>
2022-04-23 15:36:37 -0700

    deleted dist folder

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from "./client";
-import { User } from "./user";
-import { Post } from "./post";
-import { Chat } from "./chat";
-import { ClientUser } from "./clientuser";
-import { Group, GroupInviteType } from "./group";
-export type { User, ClientUser, Post, Chat, Group, GroupInviteType };
-export { Client };
